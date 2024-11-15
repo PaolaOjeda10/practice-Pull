68b790235e89848064508aba3cbd197cf8b90d37
Paola <paolaojedaaliaga10@gmail.com>
2024-11-14 21:25:37 -0400

    practica 3

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
 const props=defineProps({
     persons:{
         type:Array,
@@ -16,13 +16,27 @@ const styleBackground = computed(()=>{
         }}
     )
 })
+const styleColor = ref(['red', 'yellow', 'green']);
 </script>
 
 <template>
 
-  <div v-for="(data, index) in styleBackground" :key="index" :style="{ backgroundColor: data.backgroundColor }">
+  <div v-for="data in styleBackground" :style="{ backgroundColor: data.backgroundColor }">
+
     name: {{ data.name }} -
+
     age: {{ data.age }} - 
+
     color: {{ data.backgroundColor }}
+
+  </div>
+
+  <div v-for="(style, index) in styleColor" :key="index">
+
+    <div :style="{ backgroundColor: style }">
+
+      <p>Estilo de fondo: {{ style }}</p>
+
+    </div>
   </div>
 </template>
